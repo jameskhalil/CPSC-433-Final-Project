58ea363bec55ead1f0d84ce7f3cd541f8331205c
jameskhalil <87144837+jameskhalil@users.noreply.github.com>
2022-11-22 09:43:24 -0700

    Create FinalProject.py

---
+++ FinalProject.py
@@ -0,0 +1,21 @@
+# 433 Final Project
+# Joshua Tolentino (30112490)
+# Jae Park (10131165)
+# Jazib Khan (30115226)
+# James Khalil (30121419)
+# Sneh Patel (30086076)
+# And-tree based search method coded in python
+
+# Take in an input file and convert to understandable information
+def readInputFile(inputFile):
+    null
+
+# Apply the and-tree search method to the input file
+def andTreeSearch(inputFile):
+    null
+    
+def result(inputFile):
+    null
+    
+result(andTreeSearch(readInputFile("input.txt")))    
+# Output the results of the and-tree search method
